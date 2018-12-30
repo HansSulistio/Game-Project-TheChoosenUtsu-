a57bf1e658654cfe7b2c29247407014b3c0870d0
Hans Sulistio <hanslsulistio98@gmail.com>
2018-12-30 17:40:14 +0700

    test

---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    private Animator anim;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(JumpToScene(sceneIndex));
+
+    }
+
+    IEnumerator JumpToScene(int sceneIndex)
+    {
+        
+        yield return new WaitForSeconds(3.0f);
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    // Use this for initialization
+    void Start () {
+        anim.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
